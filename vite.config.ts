719109aba854f5cadc59092ae6d5aae303ffba5f
Mukesh Sahoo <118727823+MukeshSahoo10@users.noreply.github.com>
2024-12-12 20:12:39 +0530

    Added missing resources

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mukeshsahoo10.github.io/SRMS/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
